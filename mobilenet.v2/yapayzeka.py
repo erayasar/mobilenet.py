67eda326bfabbd503bf04244b15dfff350c0068f
erayasar <erayasar111@gmail.com>
2024-03-12 14:08:11 +0300

    add data

--- mobilenet.v2/yapayzeka.py
+++ mobilenet.v2/yapayzeka.py
@@ -1,44 +1,5 @@
-import numpy as np
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from keras.utils import plot_model
-# Örnek veri oluşturma
-# X verisi: Görüntülerin piksel değerleri
-# y verisi: Etiketler (0: normal, 1: çıplak)
-num_samples = 100
-image_shape = (64, 64, 3)  # 64x64 boyutunda renkli görüntüler
-X = np.random.rand(num_samples, *image_shape)  # 100 adet görüntü oluştur
-y = np.random.randint(2, size=(num_samples,))  # 100 adet etiket (0 veya 1)
-
-# Verileri eğitim ve test kümelerine ayırma
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Modeli oluşturma
-model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=image_shape),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')  # Sigmoid aktivasyonu ile çıktı layer'ı
-])
-
-# Modeli derleme
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',  # Çünkü iki sınıf var (çıplak, normal)
-              metrics=['accuracy'])
-
-# Modeli eğitme
-epochs = 20
-batch_size = 16
-history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
-
-# Modelin performansını değerlendirme
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print('Test accuracy:', test_acc)
-
-# Modeli görselleştirme
-plot_model(model, to_file='model_plot2.png', show_shapes=True, show_layer_names=True)
